0f9533bbf9e414887ba21201cad1bbcb59c4a9ad
singh124rt-beep <singh124rt@gmail.com>
2026-04-05 23:18:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -9,5 +7,11 @@ const client = new Client({
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
+
+// check token exists
+if (!process.env.TOKEN) {
+  console.error("❌ TOKEN missing!");
+  process.exit(1);
+}
 
 client.login(process.env.MTQ4OTkwOTI5MTY0ODU1Mjk3MQ.GJqakx.JPldV3GFEVew5hNhf_M_Y2QxGP-fmPxIza9o5E);
