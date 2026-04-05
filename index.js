52b44c172048277b8a1feaa401fdf421e17210d9
singh124rt-beep <singh124rt@gmail.com>
2026-04-05 18:02:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.MTQ4OTkwOTI5MTY0ODU1Mjk3MQ.GJqakx.JPldV3GFEVew5hNhf_M_Y2QxGP-fmPxIza9o5E);
