4aa9712ced2840b2733985ebe8ccecafa08abd5b
singh124rt-beep <singh124rt@gmail.com>
2026-04-06 00:36:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-console.log("Bot starting...");
-
-if (!process.env.MTQ4OTkwOTI5MTY0ODU1Mjk3MQ.GJqakx.JPldV3GFEVew5hNhf_M_Y2QxGP-fmPxIza9o5E) {
-  console.log("❌ TOKEN NOT FOUND");
-} else {
-  console.log("✅ TOKEN FOUND");
-}
+console.log("HELLO BOT RUNNING");
