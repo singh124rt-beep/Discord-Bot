9f54fc2a02eabd5c891d8db91b61aed55f3c2ff4
singh124rt-beep <singh124rt@gmail.com>
2026-04-05 23:34:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+console.log("Bot starting...");
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
-// check token exists
-if (!process.env.TOKEN) {
-  console.error("❌ TOKEN missing!");
-  process.exit(1);
+if (!process.env.MTQ4OTkwOTI5MTY0ODU1Mjk3MQ.GJqakx.JPldV3GFEVew5hNhf_M_Y2QxGP-fmPxIza9o5E) {
+  console.log("❌ TOKEN NOT FOUND");
+} else {
+  console.log("✅ TOKEN FOUND");
 }
-
-client.login(process.env.MTQ4OTkwOTI5MTY0ODU1Mjk3MQ.GJqakx.JPldV3GFEVew5hNhf_M_Y2QxGP-fmPxIza9o5E);
