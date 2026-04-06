554d4345088adb281cb69fe45f9d2f089581baa3
singh124rt-beep <singh124rt@gmail.com>
2026-04-06 06:50:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// index.js
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require("express"); // add Express
+const app = express();
 
+// Dummy web server to keep Render happy
+app.get("/", (req, res) => res.send("Bot is running!"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Discord bot code
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
