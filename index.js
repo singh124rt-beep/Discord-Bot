81262ec7e7ff0c2086bd1765132605b330be1f89
singh124rt-beep <singh124rt@gmail.com>
2026-04-26 11:18:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ const commands = [
 ].map(c=>c.toJSON());
 
 // ===== READY =====
-client.once("ready", async ()=>{
+client.once("clientReady", async () => {
   console.log("🟢 Logged in as " + client.user.tag);
 
   const rest = new REST({version:"10"}).setToken(process.env.DISCORD_BOT_TOKEN);
